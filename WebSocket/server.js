8e0b23603a1b08d94a352004bcb85e1bc93f881a
guillaume corbet <guillaumecorbet18@gmail.com>
2023-09-11 20:42:03 +0200

    create room web socket working

--- WebSocket/server.js
+++ WebSocket/server.js
@@ -1,23 +1,22 @@
 const {instrument} = require("@socket.io/admin-ui");
 const dotenv = require('dotenv');
-const jwt = require('jsonwebtoken');
 dotenv.config();
-const io = require("socket.io")(process.env.ENV == "production" ? process.env.WEB_SOCKET_PROD_PORT : process.env.WEB_SOCKET_DEV_PORT, {
+const io = require("socket.io")(4000, {
     cors: {
-        origin: ["*"],
+        origin: ["http://localhost:3000"],
     },
 })
 
-const userConnected = [
+let userConnected = [
 
 ];
 
-const room = [
+let room = [
 
 ];
 
-const addUser = (socketId, server) => {
-    userConnected.push({socketId: socketId, server: server});
+const addUser = (socketId) => {
+    userConnected.push({socketId: socketId});
     return;
 }
 
@@ -32,11 +31,21 @@ const addRoom = (socketId, code) => {
     return;
 }
 
+const joinRoom = (socketId, code) => {
+    for (var i = 0; i < room.length; i++) {
+        if (room.at(i).code == code) {
+            room.at(i).user.push({id: socketId, server: false})
+            return room.at(i);
+        }
+    }
+    return "No Room";
+}
+
 const generateCodeRoom = () => {
     let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
     let find = true
     let result = ""
-    while (!true) {
+    while (find) {
         find = false
         result = ""
         for ( var i = 0; i < 5; i++ ) {
@@ -54,15 +63,26 @@ io.on("connection", (socket) => {
     console.log("a user connected");
 
     socket.on("Connect", (server) => {
-        addUser(socket.id, server);
-        console.log(server)
-        socket.emit("Connected", {token: tokenWs});
+        addUser(socket.id);
+        socket.emit("Connected");
     })
 
     socket.on("createRoom", () => {
         let code = generateCodeRoom();
         addRoom(socket.id, code);
+        socket.join(code)
         socket.emit("RoomCreated", {code: code})
+    })
+
+    socket.on("joinRoom", (code) => {
+        res = joinRoom(socket.id, code);
+        console.log(res)
+        if (res == "No Room") {
+            socket.emit("NoRoom")
+        } else {
+            socket.join(code)
+            socket.emit("RoomJoined", {res: "Oui bien sur"})
+        }
     })
 
     socket.on("disconnect", () => {
@@ -73,4 +93,4 @@ io.on("connection", (socket) => {
 
 instrument(io, {auth: false})
 console.log("Server listening port ");
-console.log(process.env.ENV == "production" ? process.env.WEB_SOCKET_PROD_PORT : process.env.WEB_SOCKET_DEV_PORT);
+console.log(4000);
